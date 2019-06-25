c77eff8b3006928d171518358bca57d8cd0ffc89
zeez <zeezrdq@gmail.com>
2019-06-25 05:49:14 +0200

    added dijsktra's algorithm and added the function to get the closeness Centrality

--- Graph.h
+++ Graph.h
@@ -10,49 +10,49 @@ using namespace std;
 class graph
 {
 public:
-	graph(int no_nodes)
-	{
-		//resize the vector to fit the no of given nodes
-		adjacency_list.resize(no_nodes);
-		no_connected_nodes.resize(no_nodes);
-	}
+    graph(int no_nodes)
+    {
+        //resize the vector to fit the no of given nodes
+        adjacency_list.resize(no_nodes);
+        no_connected_nodes.resize(no_nodes);
+        nodes_no = no_nodes;
+    }
 
-	void add_edge(int node1, int node2, int weight)
-	{
-		// since it is undirected graph, nodes are added in both lists
-		adjacency_list[node1].push_back({ node2, weight });
-		adjacency_list[node2].push_back({ node1, weight });
-		//incrementing no of nodes in each list to get "DEGREE CENTRALITY"
-		no_connected_nodes[node1]++;
-		no_connected_nodes[node2]++;
-	}
+    void add_edge(int node1, int node2, int weight)
+    {
+        // since it is undirected graph, nodes are added in both lists
+        adjacency_list[node1].push_back({node2, weight});
+        adjacency_list[node2].push_back({node1, weight});
+        //incrementing no of nodes in each list to get "DEGREE CENTRALITY"
+        no_connected_nodes[node1]++;
+        no_connected_nodes[node2]++;
+    }
 
-	void degree_centrality();
-	void closeness_centrality();
-	void betweeness_centrality();
+    void degree_centrality();
+    void closeness_centrality();
+    void betweeness_centrality();
+    void shortest_Path_closeness(int source, vector<int> &shortest_distances);
 
 private:
-	/*
-	adjacency list stores nodes in a vector of lists 
-	where each index resembles a node
-	lists of pairs to indicate an edge with node x & weight y
+    /*
+    adjacency list stores nodes in a vector of lists
+    where each index resembles a node
+    lists of pairs to indicate an edge with node x & weight y
 
-	-----------------------------------------------------------
+    -----------------------------------------------------------
 
 
-	node_index   (node_index, edge_weight),...
-	0:			 (1,5), (2,10), (3,6)
-	1:			 (0,5), (3,8)
-	2:			 (1,10)
-	3:			 (0,6), (1,8)
-	*/
+    node_index   (node_index, edge_weight),...
+    0:			 (1,5), (2,10), (3,6)
+    1:			 (0,5), (3,8)
+    2:			 (1,10)
+    3:			 (0,6), (1,8)
+    */
 
-	vector<list<pair<int, int>>> adjacency_list;
-	vector<int> no_connected_nodes;
+    vector<list<pair<int, int>>>
+    adjacency_list;
+    vector<int> no_connected_nodes;
+    int nodes_no;
 };
 
-
 #endif // !GRAPH
-
-
-
--- closeness.cpp
+++ closeness.cpp
@@ -1,6 +1,57 @@
 #include "Graph.h"
+#include <queue>
+#include <vector>
+#include <list>
+#include <iomanip>
+void graph::shortest_Path_closeness(int source, vector<int> &shortest_distances)
+{
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> p_queue;
+
+    p_queue.push({0, source});
+    shortest_distances[source] = 0;
+
+    list<pair<int, int>>::iterator it;
+
+    while (!(p_queue.empty()))
+    {
+
+        int next_node = p_queue.top().second;
+        int weight = p_queue.top().first;
+
+        p_queue.pop();
+
+        for (it = this->adjacency_list[next_node].begin(); it != this->adjacency_list[next_node].end(); it++)
+        {
+
+            int adj_node = it->first;
+            int adj_node_weight = it->second;
+
+            if (shortest_distances[adj_node] > shortest_distances[next_node] + adj_node_weight)
+            {
+
+                shortest_distances[adj_node] = shortest_distances[next_node] + adj_node_weight;
+                p_queue.push({shortest_distances[adj_node], adj_node});
+            }
+        }
+    }
+}
 
 void graph::closeness_centrality()
 {
 
+    float sum;
+    for (int k = 0; k < this->nodes_no; ++k)
+    {
+        sum = 0;
+        vector<int> shortest_distances(this->nodes_no, 100000);
+        this->shortest_Path_closeness(k, shortest_distances);
+        // cout << "entering node " << k << endl;
+        for (int i = 0; i < this->nodes_no; i++)
+        {
+            // cout << shortest_distances[i] << endl;
+            sum += shortest_distances[i];
+        }
+        cout << setprecision(8) << (this->nodes_no - 1) / sum << endl;
+    }
 }
